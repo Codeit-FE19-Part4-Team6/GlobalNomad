18a51911d72e89f9fe1d99eac2d5ecc03636a108
apple <apple@apples-MacBook-Air.local>
2025-12-22 01:15:28 +0900

    Fix: 메인페이지 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,7 @@
-import IntroImageSection from '@/components/common/image-upload/IntroImageSection';
-
 const MainPage = () => {
   return (
     <>
       <h2>메인 페이지 입니다</h2>
-      <IntroImageSection />
     </>
   );
 };
