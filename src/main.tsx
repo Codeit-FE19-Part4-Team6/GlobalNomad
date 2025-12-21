843e7260db2f0836ea3c7c95d9e75e3e1a8ea0cb
apple <apple@apples-MacBook-Air.local>
2025-12-22 01:10:41 +0900

    Fix: strictmod 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { StrictMode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/common/Layout.tsx';
@@ -32,7 +33,9 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
 );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,10 @@
+import IntroImageSection from '@/components/common/image-upload/IntroImageSection';
+
 const MainPage = () => {
   return (
     <>
       <h2>메인 페이지 입니다</h2>
+      <IntroImageSection />
     </>
   );
 };
