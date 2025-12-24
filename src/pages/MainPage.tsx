c9b832285b359b96b05e5e2c0b7b12806c555b27
ky7408 <ky7408@naver.com>
2025-12-24 14:26:47 +0900

    Feat: 중간작업

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,11 @@
+import ReservationInfoModalTestPage from '@/pages/ReservationTest';
+
 const MainPage = () => {
   return (
     <>
       <h2>메인 페이지 입니다</h2>
+
+      <ReservationInfoModalTestPage />
     </>
   );
 };
