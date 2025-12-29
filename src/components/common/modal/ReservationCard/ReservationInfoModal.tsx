441e3209cbaa2c7ff3f07222c2c248fe39f47bec
ky7408 <ky7408@naver.com>
2025-12-29 16:55:27 +0900

    Refactor: 충돌수정

--- src/components/common/modal/ReservationCard/ReservationInfoModal.tsx
+++ src/components/common/modal/ReservationCard/ReservationInfoModal.tsx
@@ -155,7 +155,7 @@ export default function ReservationInfoModal({
                   headCount={r.headCount}
                   tab={tab}
                   onApprove={() => console.log('승인', r.id)} //임시코드 승인하기 api함수 넘겨주세요
-                  onReject={() => console.log('거절', r.id)} //임시코드  거절하기 api함수 넘겨주세요
+                  onReject={() => console.log('거절', r.id)} //임시코드  거절하기 api함수 넘겨주세요 제거예정
                 />
               ))
             )}
