bd7f7a27740524a1b5fec5d805110d0f7a68d5da
apple <apple@apples-MacBook-Air.local>
2025-12-30 17:01:30 +0900

    Fix: 아바타 파일 위치 변경

--- src/components/common/Header/HeaderUserMenu.tsx
+++ src/components/common/Header/HeaderUserMenu.tsx
@@ -1,5 +1,5 @@
 import Icons from '@/assets/icons';
-import Avatar from '@/components/Avatar';
+import Avatar from '@/components/common/Avatar';
 
 import { LogOut } from 'lucide-react';
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,7 +5,7 @@ import MyPageLayout from '@/pages/MyPageLayout';
 const MainPage = () => {
   return (
     <>
-      <h2>메인 페이지 입니다</h2>
+      {/* <h2>메인 페이지 입니다</h2> */}
       <MyPageLayout />
       {/* <CreateActivityPage /> */}
     </>
