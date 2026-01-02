0c83a0ff9d63bb80a4b3cf580e1d8fe87e3732c4
le2yunji <162525231+le2yunji@users.noreply.github.com>
2026-01-02 20:26:56 +0900

    Remove unused http import in useSignupForm

--- src/hooks/useSignupForm.ts
+++ src/hooks/useSignupForm.ts
@@ -12,7 +12,6 @@ import {
   isValidNickname,
   isValidPassword,
 } from '@/utils/validation.utils';
-import { http } from '@/apis/http';
 import usersApi from '@/apis/users';
 
 export interface SignupFormInputs {
